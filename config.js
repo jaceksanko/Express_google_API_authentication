9bef54a6cdd604a774a4ac99f3d9ce7791c4d58e
jaceksanko <jacek.sanko@gmail.com>
2018-04-26 15:47:57 +0200

    Add google login witch Passport-google-oauth

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '20729864525-i75une1n63gcaui8mp919lf8cjeh6q17.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'xhrNX3knh5raillmQYiMWB6E',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
